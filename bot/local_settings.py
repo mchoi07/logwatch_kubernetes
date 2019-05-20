3d04bd6778398d11dff474443ce479f8fe8662eb
Minkyu Choi <nathanael.m.choi@gmail.com>
2019-05-20 16:03:52 -0400

    Update local_settings.py

--- bot/local_settings.py
+++ bot/local_settings.py
@@ -1,4 +1,4 @@
-SLACK_API_TOKEN="xoxb-627883246647-637031909204-vLS3z5fI13rsRkZ3Q8JgxWqF"
+SLACK_API_TOKEN=" "
 PLUGINS = [
     'machine.plugins.builtin.general.PingPongPlugin',
     'plugins.log.LoggerPlugin'
